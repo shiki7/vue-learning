c464a4274afd8b51a0c83d2fbbcce2e79246c12b
shiki7 <bitbite8@gmail.com>
2019-02-11 14:53:32 +0900

    component

--- hello_app/src/App.vue
+++ hello_app/src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-bind:title="message" v-on:result-event="appAction"/>
+    <hr>
+    <p>{{result}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      message: "Hello!",
+      result: ""
+    };
+  },
+  methods: {
+    appAction: function(message) {
+      this.result = '(*** you send:)"' + message + '". ***)';
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
