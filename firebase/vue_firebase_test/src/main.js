bd7611d6bf2f286424f7bea0e5a35b57e67a5011
shiki7 <bitbite8@gmail.com>
2019-03-02 07:37:28 +0900

    delete

--- firebase/vue_firebase_test/src/main.js
+++ firebase/vue_firebase_test/src/main.js
@@ -9,12 +9,12 @@ Vue.config.productionTip = false
 
 // Initialize Firebase
 var config = {
-  apiKey: 'AIzaSyAJ4tDqNm7Eixd8-vp1VPeWGUwwgumEzU8',
-  authDomain: 'vue-test-5b634.firebaseapp.com',
-  databaseURL: 'https://vue-test-5b634.firebaseio.com',
-  projectId: 'vue-test-5b634',
-  storageBucket: 'vue-test-5b634.appspot.com',
-  messagingSenderId: '506810547531'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 firebase.initializeApp(config)
 
